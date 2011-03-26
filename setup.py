b0947ee28d867d4727cec7b19c03e69936b2b505
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-03-26 18:20:31 +0100

    bump the version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name="lojban-teachparse",
-        version="0.02",
+        version="0.03",
         scripts=["parser.py",
                  "run.py"],
         install_requires = ["flask", "lojbansuggest", "lojbantools"]
